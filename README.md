c136c10b19568320c6ace7cfb1ddca8df79b1a4e
Bashir-Danish <bashirdanish124@gmail.com>
2023-05-02 18:41:34 +0430

    all done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# magic_ball
+# flutter_app
 
 A new Flutter project.
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: null,
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BallPage(),
     ),
   );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text("Ask Me Anything !"),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: Image.asset("images/ball$ballNumber.png"),
+        onPressed: () {
+          print('I got clicked');
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+      ),
+    );
+  }
 }
